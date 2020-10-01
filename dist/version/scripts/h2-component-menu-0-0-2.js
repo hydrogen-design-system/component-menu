9c00331d0eace056a8b511997ab147f4dace5cc1
Josh Beveridge <josh.beveridge@outlook.com>
2020-10-01 11:25:25 -0400

    Fixes a bug with the system build files that prevented proper importing.

--- dist/version/scripts/h2-component-menu-0-0-2.js
+++ dist/version/scripts/h2-component-menu-0-0-2.js
@@ -1,12 +1,12 @@
 // Hydrogen / Component / Scripts
 
 // Focusable Items
-function h2ComMenFocusable001(focusElement) {
+function h2ComMenFocusable002(focusElement) {
     return (focusElement.find('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'));
 }
 
 // "hasAttr" Function
-$.fn.h2ComMenHasAttr001 = function (name) {
+$.fn.h2ComMenHasAttr002 = function (name) {
     var attr = $(this).attr(name);
     return (typeof attr !== typeof undefined && attr !== false);
 };
@@ -16,7 +16,7 @@ $.fn.h2ComMenHasAttr001 = function (name) {
     // Mobile Menu Tabbing
 
         // Tab Loop Function
-        function h2ComMenMobileTabLoop001(e, menuItems, menuItemCount) {
+        function h2ComMenMobileTabLoop002(e, menuItems, menuItemCount) {
             var keyCode = e.keyCode || e.which;
             if (keyCode == 9 && !e.shiftKey) {
                 e.preventDefault();
@@ -51,18 +51,18 @@ $.fn.h2ComMenHasAttr001 = function (name) {
         }
 
         // Tab Loop Trigger
-        function h2ComMenMobileMenuTabLoopTrigger001(menuItems) {
+        function h2ComMenMobileMenuTabLoopTrigger002(menuItems) {
             // Trigger the tab loop event listener on the menu items.
             var menuItemCount = $(menuItems).length - 1; // Checks the number of menu items in the array.
             $(menuItems).on("keydown.mobilemenutabloop", function(e) {
-                h2ComMenMobileTabLoop001(e, menuItems, menuItemCount);
+                h2ComMenMobileTabLoop002(e, menuItems, menuItemCount);
             });
         }
     
     // Mobile Menu Escape Events
 
         // Escape Function
-        function h2ComMenMobileMenuEscape001(e, trigger, menu, menuItems) {
+        function h2ComMenMobileMenuEscape002(e, trigger, menu, menuItems) {
             var keyCode = e.keyCode || e.which;
             if (keyCode == 27) {
                 // Remove the current submenu's tab loop and escape event listeners.
@@ -80,19 +80,19 @@ $.fn.h2ComMenHasAttr001 = function (name) {
         }
 
         // Escape Trigger
-        function h2ComMenMobileMenuEscapeTrigger001(trigger, menu, menuItems) {
+        function h2ComMenMobileMenuEscapeTrigger002(trigger, menu, menuItems) {
             // Trigger the escape event listener on the submenu items.
             $(menuItems).on("keydown.mobilemenuescape", function(e) {
-                h2ComMenMobileMenuEscape001(e, trigger, menu, menuItems);
+                h2ComMenMobileMenuEscape002(e, trigger, menu, menuItems);
             });
         }
 
     // Mobile Menu Trigger
 
         // Trigger Function
-        function h2ComMenMobileMenuToggle001(trigger) {
+        function h2ComMenMobileMenuToggle002(trigger) {
             // Create a variable for the menu.
-            var menu = $("[data-h2-menu-wrapper-0-0-1] [data-h2-menu]");
+            var menu = $("[data-h2-menu-wrapper-0-0-2] [data-h2-menu]");
             // Find available menu items and buttons.
             var menuItems = [];
             $(menu).children("[data-h2-main-menu]").children("li").each(function() {
@@ -125,21 +125,21 @@ $.fn.h2ComMenHasAttr001 = function (name) {
                 // Activate the menu.
                 $(menu).addClass("h2-mobile-menu-active");
                 // Enable the tab loop and escape event listeners
-                h2ComMenMobileMenuTabLoopTrigger001(menuItems);
-                h2ComMenMobileMenuEscapeTrigger001(trigger, menu, menuItems);
+                h2ComMenMobileMenuTabLoopTrigger002(menuItems);
+                h2ComMenMobileMenuEscapeTrigger002(trigger, menu, menuItems);
             }
         }
 
         // Event Listener
-        $("[data-h2-menu-wrapper-0-0-1] [data-h2-mobile-menu-trigger]").on("click", function(e) {
+        $("[data-h2-menu-wrapper-0-0-2] [data-h2-mobile-menu-trigger]").on("click", function(e) {
             e.preventDefault();
-            h2ComMenMobileMenuToggle001(this);
+            h2ComMenMobileMenuToggle002(this);
         });
 
     // Mobile Menu Page Anchor Handler
     // This script closes the menu and scrolls the user to the anchor if the menu item they've clicked on directs them to a point on their current page.
-    function h2ComMenMobileMenuAnchorClick001(trigger) {
-        if ($("[data-h2-menu-wrapper-0-0-1] [data-h2-menu]").hasClass("h2-mobile-menu-active")) {
+    function h2ComMenMobileMenuAnchorClick002(trigger) {
+        if ($("[data-h2-menu-wrapper-0-0-2] [data-h2-menu]").hasClass("h2-mobile-menu-active")) {
             // Set anchor destination.
             var destination = $(trigger).attr("href");
             if (destination.match("^#")) {
@@ -168,8 +168,8 @@ $.fn.h2ComMenHasAttr001 = function (name) {
         }
     }
 
-    $("[data-h2-menu-wrapper-0-0-1] [data-h2-menu] [role='menuitem']").on("click", function(e) {
-        h2ComMenMobileMenuAnchorClick001(this);
+    $("[data-h2-menu-wrapper-0-0-2] [data-h2-menu] [role='menuitem']").on("click", function(e) {
+        h2ComMenMobileMenuAnchorClick002(this);
     });
 
 // Desktop Menu Scripts
@@ -190,7 +190,7 @@ $.fn.h2ComMenHasAttr001 = function (name) {
     </ul> */}
 
     // Submenu Tab Loop
-    function h2ComMenSubmenuTabLoop001(e, submenuItems, submenuItemCount) {
+    function h2ComMenSubmenuTabLoop002(e, submenuItems, submenuItemCount) {
         var keyCode = e.keyCode || e.which;
         if (keyCode == 9 && !e.shiftKey) {
             e.preventDefault();
@@ -224,7 +224,7 @@ $.fn.h2ComMenHasAttr001 = function (name) {
         }
     }
 
-    function h2ComMenSubmenuTabLoopTrigger001(trigger, submenu) {
+    function h2ComMenSubmenuTabLoopTrigger002(trigger, submenu) {
         // Create a list of submenu items.
         var submenuItems = []; // Creates an empty array.
         $(submenu).children("li").each(function() {
@@ -240,13 +240,13 @@ $.fn.h2ComMenHasAttr001 = function (name) {
         // Trigger the tab loop event listener on the submenu items.
         var submenuItemCount = $(submenuItems).length - 1; // Checks the number of submenu items in the array.
         $(submenuItems).on("keydown.submenutabloop", function(e) {
-            h2ComMenSubmenuTabLoop001(e, submenuItems, submenuItemCount);
+            h2ComMenSubmenuTabLoop002(e, submenuItems, submenuItemCount);
         });
 
     }
 
     // Submenu Escape Loop
-    function h2ComMenSubmenuEscape001(e, trigger, parentMenuItem, parentSubmenu, parentSubmenuTrigger, submenu, submenuItems) {
+    function h2ComMenSubmenuEscape002(e, trigger, parentMenuItem, parentSubmenu, parentSubmenuTrigger, submenu, submenuItems) {
         var keyCode = e.keyCode || e.which;
         if (keyCode == 27) {
             // Remove the current submenu's tab loop and escape event listeners.
@@ -261,9 +261,9 @@ $.fn.h2ComMenHasAttr001 = function (name) {
             // Focus the submenu's trigger.
             $(trigger)[0].focus();
             // Add the tab loop and escape event listeners to the parent submenu IF the parent is a submenu.
-            if ($(parentSubmenu).h2ComMenHasAttr001("data-h2-submenu") == true) {
-                h2ComMenSubmenuTabLoopTrigger001(parentSubmenuTrigger, parentSubmenu);
-                h2ComMenSubmenuEscapeTrigger001(parentSubmenuTrigger);
+            if ($(parentSubmenu).h2ComMenHasAttr002("data-h2-submenu") == true) {
+                h2ComMenSubmenuTabLoopTrigger002(parentSubmenuTrigger, parentSubmenu);
+                h2ComMenSubmenuEscapeTrigger002(parentSubmenuTrigger);
             }
             // Otherwise check to see if the parent is the main menu.
             else if ($(parentSubmenu).closest("[data-h2-menu]").hasClass("h2-mobile-menu-active")) {
@@ -280,16 +280,16 @@ $.fn.h2ComMenHasAttr001 = function (name) {
                 // Set the menu.
                 var menu = $(parentSubmenu).closest("[data-h2-menu]");
                 // Set the mobile menu trigger.
-                var mobileTrigger = $(parentSubmenu).closest("[data-h2-menu-wrapper-0-0-1]").find("[data-h2-mobile-menu-trigger]");
+                var mobileTrigger = $(parentSubmenu).closest("[data-h2-menu-wrapper-0-0-2]").find("[data-h2-mobile-menu-trigger]");
                 menuItems = $(menuItems).add($(mobileTrigger));
                 // Enable the tab loop and escape event listeners.
-                h2ComMenMobileMenuTabLoopTrigger001(menuItems);
-                h2ComMenMobileMenuEscapeTrigger001(mobileTrigger, menu, menuItems);
+                h2ComMenMobileMenuTabLoopTrigger002(menuItems);
+                h2ComMenMobileMenuEscapeTrigger002(mobileTrigger, menu, menuItems);
             }
         }
     }
 
-    function h2ComMenSubmenuEscapeTrigger001(trigger) {
+    function h2ComMenSubmenuEscapeTrigger002(trigger) {
         // Set hierarchy variables.
         var parentMenuItem = $(trigger).closest("li");
         var parentSubmenu = $(parentMenuItem).parent();
@@ -308,14 +308,14 @@ $.fn.h2ComMenHasAttr001 = function (name) {
         submenuItems = $(submenuItems).add($(trigger));
         // Trigger the escape event listener on the submenu items.
         $(submenuItems).on("keydown.submenuescape", function(e) {
-            h2ComMenSubmenuEscape001(e, trigger, parentMenuItem, parentSubmenu, parentSubmenuTrigger, submenu, submenuItems);
+            h2ComMenSubmenuEscape002(e, trigger, parentMenuItem, parentSubmenu, parentSubmenuTrigger, submenu, submenuItems);
         });
     }
 
     // Submenu Trigger
 
         // Trigger Function
-        function h2ComMenSubmenuToggle001(trigger) {
+        function h2ComMenSubmenuToggle002(trigger) {
             
             var parentMenuItem = $(trigger).closest("li");
             var parentSubmenu = $(parentMenuItem).parent();
@@ -345,9 +345,9 @@ $.fn.h2ComMenHasAttr001 = function (name) {
                 $(parentMenuItem).find("[data-h2-submenu]").removeClass("h2-active");
                 $(parentMenuItem).find("li").removeClass("h2-active");
                 // Check to see if the parent is a submenu, and turn on tabbing.
-                if ($(parentSubmenu).h2ComMenHasAttr001("data-h2-submenu") == true) {
-                    h2ComMenSubmenuTabLoopTrigger001(parentSubmenuTrigger, parentSubmenu);
-                    h2ComMenSubmenuEscapeTrigger001(parentSubmenuTrigger);
+                if ($(parentSubmenu).h2ComMenHasAttr002("data-h2-submenu") == true) {
+                    h2ComMenSubmenuTabLoopTrigger002(parentSubmenuTrigger, parentSubmenu);
+                    h2ComMenSubmenuEscapeTrigger002(parentSubmenuTrigger);
                 }
                 // Otherwise check to see if the parent menu is the main menu.
                 else if ($(parentSubmenu).closest("[data-h2-menu]").hasClass("h2-mobile-menu-active")) {
@@ -364,17 +364,17 @@ $.fn.h2ComMenHasAttr001 = function (name) {
                     // Set the menu.
                     var menu = $(parentSubmenu).closest("[data-h2-menu]");
                     // Set the mobile menu trigger.
-                    var mobileTrigger = $(parentSubmenu).closest("[data-h2-menu-wrapper-0-0-1]").find("[data-h2-mobile-menu-trigger]");
+                    var mobileTrigger = $(parentSubmenu).closest("[data-h2-menu-wrapper-0-0-2]").find("[data-h2-mobile-menu-trigger]");
                     menuItems = $(menuItems).add($(mobileTrigger));
                     // Enable the tab loop and escape event listeners.
-                    h2ComMenMobileMenuTabLoopTrigger001(menuItems);
-                    h2ComMenMobileMenuEscapeTrigger001(mobileTrigger, menu, menuItems);
+                    h2ComMenMobileMenuTabLoopTrigger002(menuItems);
+                    h2ComMenMobileMenuEscapeTrigger002(mobileTrigger, menu, menuItems);
                 }
             }
             // If the submenu isn't active, open it.
             else {
                 // Remove the tab loop and escape event listeners on the parent submenu IF it's a submenu.
-                if ($(parentSubmenu).h2ComMenHasAttr001("data-h2-submenu") == true) {
+                if ($(parentSubmenu).h2ComMenHasAttr002("data-h2-submenu") == true) {
                     var submenuItems = [];
                     $(parentSubmenu).children("li").each(function() {
                         if ($(this).children("[role='menuitem']")) {
@@ -402,7 +402,7 @@ $.fn.h2ComMenHasAttr001 = function (name) {
                         }
                     });
                     // Set the mobile menu trigger.
-                    var mobileTrigger = $(parentSubmenu).closest("[data-h2-menu-wrapper-0-0-1]").find("[data-h2-mobile-menu-trigger]");
+                    var mobileTrigger = $(parentSubmenu).closest("[data-h2-menu-wrapper-0-0-2]").find("[data-h2-mobile-menu-trigger]");
                     menuItems = $(menuItems).add($(mobileTrigger));
                     // Remove the tab loop and escape event listeners.
                     $(menuItems).off("keydown.mobilemenutabloop");
@@ -413,15 +413,15 @@ $.fn.h2ComMenHasAttr001 = function (name) {
                 $(parentMenuItem).addClass("h2-active");
                 $(submenu).addClass("h2-active");
                 // Add the tab loop event listeners to the newly opened submenu.
-                h2ComMenSubmenuTabLoopTrigger001(trigger, submenu);
+                h2ComMenSubmenuTabLoopTrigger002(trigger, submenu);
                 // Add the escape event listeners to the newly opened submeny.
-                h2ComMenSubmenuEscapeTrigger001(trigger);
+                h2ComMenSubmenuEscapeTrigger002(trigger);
             }
 
         }
 
         // Trigger Event
-        $("[data-h2-menu-wrapper-0-0-1] [data-h2-submenu-trigger]").on("click.submenutrigger", function(e) {
+        $("[data-h2-menu-wrapper-0-0-2] [data-h2-submenu-trigger]").on("click.submenutrigger", function(e) {
             e.preventDefault();
-            h2ComMenSubmenuToggle001(this);
+            h2ComMenSubmenuToggle002(this);
         });
